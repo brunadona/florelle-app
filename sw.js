faa98dcf34cbb17454912be3c1dba6eeafebfcba
brunadona <brunadonaa@gmail.com>
2026-05-18 16:23:15 -0300

    fix: force sw cache bust 20260518192315

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-// 20260518024134
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => {
+// 20260518192315
+self.addEventListener("install", e => { self.skipWaiting(); });
+self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => caches.delete(n)))));
   self.clients.claim();
 });
-self.addEventListener('fetch', e => {});
+self.addEventListener("fetch", e => {});
