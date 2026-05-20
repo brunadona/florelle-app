3b20befcb7fbe681d174b22d103a2140d668bbea
brunadona <brunadonaa@gmail.com>
2026-05-20 13:15:00 -0300

    cache bust 20260520161500

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// 20260518235716
+// 20260520161500
 self.addEventListener("install", e => { self.skipWaiting(); });
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => caches.delete(n)))));
