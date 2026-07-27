04525db9f23237ffd5bb8eaf33cb9a4af871cc16
Bruna <brunadonaa@gmail.com>
2026-07-27 19:03:25 -0300

    fix: inventário secagem mostra 'sobra para X noivas' em vez de 0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// 20260727161000
+// 20260727162000
 self.addEventListener("install", e => { self.skipWaiting(); });
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => caches.delete(n)))));
